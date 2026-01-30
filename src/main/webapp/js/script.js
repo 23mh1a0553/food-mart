a0ad06e90309c34188662acf0e7a3fbee116753f
Polanati tejaswi <23mh1a0553@acoe.edu.in>
2026-01-30 17:41:00 +0530

    Fix pom.xml and update web resources

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -1,109 +1,17 @@
-(function($) {
+(function ($) {
 
   "use strict";
 
-  var initPreloader = function() {
-    $(document).ready(function($) {
-    var Body = $('body');
-        Body.addClass('preloader-site');
-    });
-    $(window).load(function() {
-        $('.preloader-wrapper').fadeOut();
-        $('body').removeClass('preloader-site');
+  // init jarallax parallax
+  var initJarallax = function () {
+    jarallax(document.querySelectorAll(".jarallax"));
+
+    jarallax(document.querySelectorAll(".jarallax-img"), {
+      keepImg: true,
     });
   }
 
-  // init Chocolat light box
-	var initChocolat = function() {
-		Chocolat(document.querySelectorAll('.image-link'), {
-		  imageSize: 'contain',
-		  loop: true,
-		})
-	}
-
-  var initSwiper = function() {
-
-    var swiper = new Swiper(".main-swiper", {
-      speed: 500,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-    });
-
-    var category_swiper = new Swiper(".category-carousel", {
-      slidesPerView: 6,
-      spaceBetween: 30,
-      speed: 500,
-      navigation: {
-        nextEl: ".category-carousel-next",
-        prevEl: ".category-carousel-prev",
-      },
-      breakpoints: {
-        0: {
-          slidesPerView: 2,
-        },
-        768: {
-          slidesPerView: 3,
-        },
-        991: {
-          slidesPerView: 4,
-        },
-        1500: {
-          slidesPerView: 6,
-        },
-      }
-    });
-
-    var brand_swiper = new Swiper(".brand-carousel", {
-      slidesPerView: 4,
-      spaceBetween: 30,
-      speed: 500,
-      navigation: {
-        nextEl: ".brand-carousel-next",
-        prevEl: ".brand-carousel-prev",
-      },
-      breakpoints: {
-        0: {
-          slidesPerView: 2,
-        },
-        768: {
-          slidesPerView: 2,
-        },
-        991: {
-          slidesPerView: 3,
-        },
-        1500: {
-          slidesPerView: 4,
-        },
-      }
-    });
-
-    var products_swiper = new Swiper(".products-carousel", {
-      slidesPerView: 5,
-      spaceBetween: 30,
-      speed: 500,
-      navigation: {
-        nextEl: ".products-carousel-next",
-        prevEl: ".products-carousel-prev",
-      },
-      breakpoints: {
-        0: {
-          slidesPerView: 1,
-        },
-        768: {
-          slidesPerView: 3,
-        },
-        991: {
-          slidesPerView: 4,
-        },
-        1500: {
-          slidesPerView: 6,
-        },
-      }
-    });
-  }
-
+  // input spinner
   var initProductQty = function(){
 
     $('.product-qty').each(function(){
@@ -113,15 +21,15 @@
 
       $el_product.find('.quantity-right-plus').click(function(e){
           e.preventDefault();
-          var quantity = parseInt($el_product.find('#quantity').val());
-          $el_product.find('#quantity').val(quantity + 1);
+          var quantity = parseInt($el_product.find('.quantity').val());
+          $el_product.find('.quantity').val(quantity + 1);
       });
 
       $el_product.find('.quantity-left-minus').click(function(e){
           e.preventDefault();
-          var quantity = parseInt($el_product.find('#quantity').val());
+          var quantity = parseInt($el_product.find('.quantity').val());
           if(quantity>0){
-            $el_product.find('#quantity').val(quantity - 1);
+            $el_product.find('.quantity').val(quantity - 1);
           }
       });
 
@@ -129,24 +37,137 @@
 
   }
 
-  // init jarallax parallax
-  var initJarallax = function() {
-    jarallax(document.querySelectorAll(".jarallax"));
+  // init Chocolat light box
+	var initChocolat = function () {
+		Chocolat(document.querySelectorAll('.image-link'), {
+			imageSize: 'contain',
+			loop: true,
+		})
+	}
 
-    jarallax(document.querySelectorAll(".jarallax-keep-img"), {
-      keepImg: true,
+  // Animate Texts
+  var initTextFx = function () {
+    $('.txt-fx').each(function () {
+      var newstr = '';
+      var count = 0;
+      var delay = 0;
+      var stagger = 10;
+      var words = this.textContent.split(/\s/);
+      
+      $.each( words, function( key, value ) {
+        newstr += '<span class="word">';
+
+        for ( var i = 0, l = value.length; i < l; i++ ) {
+          newstr += "<span class='letter' style='transition-delay:"+ ( delay + stagger * count ) +"ms;'>"+ value[ i ] +"</span>";
+          count++;
+        }
+        newstr += '</span>';
+        newstr += "<span class='letter' style='transition-delay:"+ delay +"ms;'>&nbsp;</span>";
+        count++;
+      });
+
+      this.innerHTML = newstr;
     });
   }
 
-  // document ready
-  $(document).ready(function() {
-    
-    initPreloader();
-    initSwiper();
+  $(document).ready(function () {
+
     initProductQty();
     initJarallax();
     initChocolat();
+    initTextFx();
+
+    $(".user-items .search-item").click(function () {
+      $(".search-box").toggleClass('active');
+      $(".search-box .search-input").focus();
+    });
+    $(".close-button").click(function () {
+      $(".search-box").toggleClass('active');
+    });
+
+    var breakpoint = window.matchMedia('(max-width:61.93rem)');
+
+    if (breakpoint.matches === false) {
+      
+      var swiper = new Swiper(".main-swiper", {
+        slidesPerView: 1,
+        spaceBetween: 48,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        breakpoints: {
+          900: {
+            slidesPerView: 2,
+            spaceBetween: 48,
+          },
+        },
+      });
+
+      // homepage 2 slider
+      var swiper = new Swiper(".thumb-swiper", {
+        direction: 'horizontal',
+        slidesPerView: 6,
+        spaceBetween: 6,
+        breakpoints: {
+          900: {
+            direction: 'vertical',
+            spaceBetween: 6,
+          },
+        },
+      });
+      var swiper2 = new Swiper(".large-swiper", {
+        spaceBetween: 48,
+        effect: 'fade',
+        slidesPerView: 1,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        thumbs: {
+          swiper: swiper,
+        },
+      });
+
+    }
+
+    // product single page
+    var thumb_slider = new Swiper(".product-thumbnail-slider", {
+      slidesPerView: 5,
+      spaceBetween: 10,
+      // autoplay: true,
+      direction: "vertical",
+      breakpoints: {
+        0: {
+          direction: "horizontal"
+        },
+        992: {
+          direction: "vertical"
+        },
+      },
+    });
+
+    // product large
+    var large_slider = new Swiper(".product-large-slider", {
+      slidesPerView: 1,
+      // autoplay: true,
+      spaceBetween: 0,
+      effect: 'fade',
+      thumbs: {
+        swiper: thumb_slider,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+
+    
 
   }); // End of a document
+
+  $(window).load(function(){
+    $('.preloader').fadeOut();
+  });
 
 })(jQuery);
